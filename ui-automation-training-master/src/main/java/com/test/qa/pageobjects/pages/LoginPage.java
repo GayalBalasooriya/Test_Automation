b30154dfaa27f6979ed29583b38348a067f24d42
Gayal Balasooriya <baigayal01@gmail.com>
2020-09-19 20:36:44 +0530

    initial commit

---
+++ ui-automation-training-master/src/main/java/com/test/qa/pageobjects/pages/LoginPage.java
@@ -0,0 +1,45 @@
+package com.test.qa.pageobjects.pages;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+
+import com.test.qa.pageobjects.utils.PageBase;
+
+/**
+ * LoginPage.java - class to verify Login Page functions Created by SrirankanK
+ * on 10/3/2018.
+ */
+public class LoginPage extends PageBase {
+
+	public static boolean isLoginPageDisplayed() {
+		return false;
+	}
+
+	public static void setUsernamePassword(String userName, String password) {
+	}
+
+	public static void clickSubmit() {
+	}
+
+	public static boolean isAlertDisplayed() {
+		return false;
+	}
+
+	public static String getAlertContent() {
+		return null;
+	}
+
+	public static void setPassword(String password) {
+	}
+
+	public static void setUsername(String username) {
+	}
+
+	public static String getLoginAlertMessage() {
+		return null;
+	}
+
+	public static boolean isLoginAlertDisplayed() {
+		return false;
+	}
+}
---
+++ ui-automation-training-master/src/main/java/com/test/qa/pageobjects/pages/LoginSecurePage.java
@@ -0,0 +1,36 @@
+package com.test.qa.pageobjects.pages;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+
+import com.test.qa.pageobjects.utils.PageBase;
+
+/**
+ * LoginSecurePage.java - class to verify LoginSecure Page functions Created by
+ * SrirankanK on 10/3/2018.
+ */
+public class LoginSecurePage extends PageBase {
+
+	public static boolean isLoginSecurePageDisplayed() {
+		return false;
+	}
+
+	public static String getAlertContent() {
+		return null;
+	}
+
+	public static boolean isAlertDisplayed() {
+		return false;
+	}
+
+	public static void clickLogout() {
+	}
+
+	public static boolean isLoginAlertDisplayed() {
+		return false;
+	}
+
+	public static String getLoginAlertMessage() {
+		return null;
+	}
+}
